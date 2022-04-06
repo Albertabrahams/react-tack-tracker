4982541dfec9eb614af389b0858836fcf8cbdf2e
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-04-06 12:24:23 +0300

    project done

--- src/App.js
+++ src/App.js
@@ -1,55 +1,77 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Study React Pre-Class Notes",
-      day: "Dec 12th at 2:30pm",
-      isDone: false,
-    },
-    {
-      id: 2,
-      text: "Feed the Dog",
-      day: "Dec 13th at 1:30pm",
-      isDone: true,
-    },
-    {
-      id: 3,
-      text: "Attend In-Class",
-      day: "Dec 14th at 3:00pm",
-      isDone: false,
-    },
-  ]);
-
+  const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
 
-  // DELETE TASK
-  const deleteTask = (deletedTaskId) => {
-    // console.log("delete Task", deletedTaskId);
-    setTasks(tasks.filter((task) => task.id !== deletedTaskId));
-  };
+  const baseUrl = "http://localhost:5000/tasks";
 
+ //we get with fetch
+
+  // const fetchTasks = async () =>{
+  // try{
+  // const res = await fetch(baseUrl);
+  // const data = await res.json();
+  // setTasks(data);}
+  // catch(err){
+  //   console.log(err);
+  // }
+  // };
+
+  //we get with axios
+  const fetchTasks = async () => {
+    const {data} = await axios.get(baseUrl);
+    setTasks(data);
+  }
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+  
+ 
   // ADD TASK
-  const addTask = (newTask) => {
-    const id = Math.floor(Math.random() * 1000 + 1);
-    const addNewTask = { id, ...newTask };
-    setTasks([...tasks, addNewTask]);
-  };
+  // const addTask = (newTask) => {
+  //   const id = Math.floor(Math.random() * 1000 + 1);
+  //   const addNewTask = { id, ...newTask };
+  //   setTasks([...tasks, addNewTask]);
+  // };
+  const addTask = async (newTask)=>{
+    await axios.post(baseUrl, newTask);
+    fetchTasks();
+  }
+
+  // DELETE TASK
+  const deleteTask = async(deletedTaskId) =>{
+    await axios.delete(`${baseUrl}/${deletedTaskId}`);
+    fetchTasks();
+  }
+
+  // const deleteTask = (deletedTaskId) => {
+  //   // console.log("delete Task", deletedTaskId);
+  //   setTasks(tasks.filter((task) => task.id !== deletedTaskId));
+  // };
 
   // TOGGLE DONE
-  const toggleDone = (toggleDoneId) => {
-    // console.log("double click", toggleDoneId);
-    setTasks(
-      tasks.map((task) =>
-        task.id === toggleDoneId ? { ...task, isDone: !task.isDone } : task
-      )
-    );
-  };
+const toggleDone = async(toggleDoneId)=>{
+  const {data} = await axios.get(`${baseUrl}/${toggleDoneId}`);
+  const updatedTask = {...data, isDone: !data.isDone};
+  await axios.put(`${baseUrl}/${toggleDoneId}`, updatedTask);
+  //await axios.patch(`${baseUrl}/${toggleDoneId}`, {isDone:!data.isDone});
+  fetchTasks();
+}
+
+  // const toggleDone = (toggleDoneId) => {
+  //   // console.log("double click", toggleDoneId);
+  //   setTasks(
+  //     tasks.map((task) =>
+  //       task.id === toggleDoneId ? { ...task, isDone: !task.isDone } : task
+  //     )
+  //   );
+  // };
 
   // TOGGLESHOW
   const toggleShow = () => setShowAddTask(!showAddTask);
