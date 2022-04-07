bc21058ef2dce7c8e9a3afc216d029659cb2baef
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-04-07 17:03:47 +0300

    project done 2.0

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,12 +4,7 @@ const Tasks = ({ tasks, deleteTask, toggleDone }) => {
   return (
     <div>
       {tasks.map((task) => (
-        <Duty 
-        key={task.id} 
-        duty={task} 
-        deleteTask={deleteTask} 
-        toggleDone={toggleDone} 
-        />
+        <Duty duty={task} deleteTask={deleteTask} toggleDone={toggleDone} />
       ))}
     </div>
   );
